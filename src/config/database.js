79dcaaa015a7b2c8d869812f95a333c937e067b9
Clickdev <clickshop1987@gmail.com>
2026-02-15 19:51:09 +0530

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,1 +1,14 @@
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+const { JWT } = require('google-auth-library');
+const dotenv = require('dotenv');
 
+dotenv.config();
+
+let doc;
+
+function setupDatabase() {
+    console.log('Database setup initialized...');
+    // Database setup is handled in googleSheets.js
+}
+
+module.exports = { setupDatabase };
