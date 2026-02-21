782118eaf405bedd7883412c37360339d4e5d40e
Clickdev <clickshop1987@gmail.com>
2026-02-21 13:49:48 +0530

    Update index.js

--- src/commands/user/index.js
+++ src/commands/user/index.js
@@ -1,18 +1,31 @@
-// সব ইউজার কমান্ড একসাথে এক্সপোর্ট করা হবে
+/**
+ * User Commands Index
+ * Location: /src/commands/user/index.js
+ * Export all user commands
+ */
+
 const buyVoucher = require('./buyVoucher');
 const myOrders = require('./myOrders');
 const recoverVoucher = require('./recoverVoucher');
 const support = require('./support');
 const disclaimer = require('./disclaimer');
-
-// ইউজার স্টেট ম্যানেজমেন্ট
-let userState = {};
+const back = require('./back');
+const leave = require('./leave');
 
 module.exports = {
-    ...buyVoucher,
-    ...myOrders,
-    ...recoverVoucher,
-    ...support,
-    ...disclaimer,
-    userState
+  // Main commands
+  buyVoucher: (ctx) => buyVoucher(ctx),
+  myOrders: (ctx) => myOrders(ctx),
+  recoverVoucher: (ctx) => recoverVoucher(ctx),
+  support: (ctx) => support(ctx),
+  disclaimer: (ctx) => disclaimer(ctx),
+  
+  // Navigation
+  back: (ctx) => back(ctx),
+  leave: (ctx) => leave(ctx),
+  
+  // Export individual handlers for advanced use
+  buyVoucherHandler: buyVoucher,
+  recoverVoucherHandler: recoverVoucher,
+  supportHandler: support
 };
