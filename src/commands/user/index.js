d3a67979dc3d16c68a8fb954923890071d4bae27
Clickdev <clickshop1987@gmail.com>
2026-02-21 14:01:50 +0530

    Update index.js

--- src/commands/user/index.js
+++ src/commands/user/index.js
@@ -1,31 +1,15 @@
 /**
  * User Commands Index
  * Location: /src/commands/user/index.js
- * Export all user commands
+ * Simple export of all commands
  */
 
-const buyVoucher = require('./buyVoucher');
-const myOrders = require('./myOrders');
-const recoverVoucher = require('./recoverVoucher');
-const support = require('./support');
-const disclaimer = require('./disclaimer');
-const back = require('./back');
-const leave = require('./leave');
-
 module.exports = {
-  // Main commands
-  buyVoucher: (ctx) => buyVoucher(ctx),
-  myOrders: (ctx) => myOrders(ctx),
-  recoverVoucher: (ctx) => recoverVoucher(ctx),
-  support: (ctx) => support(ctx),
-  disclaimer: (ctx) => disclaimer(ctx),
-  
-  // Navigation
-  back: (ctx) => back(ctx),
-  leave: (ctx) => leave(ctx),
-  
-  // Export individual handlers for advanced use
-  buyVoucherHandler: buyVoucher,
-  recoverVoucherHandler: recoverVoucher,
-  supportHandler: support
+  buyVoucher: require('./buyVoucher'),
+  myOrders: require('./myOrders'),
+  recoverVoucher: require('./recoverVoucher'),
+  support: require('./support'),
+  disclaimer: require('./disclaimer'),
+  back: require('./back'),
+  leave: require('./leave')
 };
