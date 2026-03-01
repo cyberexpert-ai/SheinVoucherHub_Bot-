52d1492e6003541a241b6b36da5f52abf8e01308
Clickdev <clickshop1987@gmail.com>
2026-03-01 08:46:20 +0530

    Update index.js

--- src/commands/user/index.js
+++ src/commands/user/index.js
@@ -1,15 +1,43 @@
-/**
- * User Commands Index
- * Location: /src/commands/user/index.js
- * Simple export of all commands
- */
+const buyVoucher = require('./buyVoucher');
+const myOrders = require('./myOrders');
+const recoverVoucher = require('./recoverVoucher');
+const support = require('./support');
+const disclaimer = require('./disclaimer');
 
-module.exports = {
-  buyVoucher: require('./buyVoucher'),
-  myOrders: require('./myOrders'),
-  recoverVoucher: require('./recoverVoucher'),
-  support: require('./support'),
-  disclaimer: require('./disclaimer'),
-  back: require('./back'),
-  leave: require('./leave')
-};
+function register(bot) {
+  // Register callback handlers
+  bot.action('buy_voucher', async (ctx) => {
+    await buyVoucher.showCategories(ctx);
+  });
+  
+  bot.action('my_orders', async (ctx) => {
+    await myOrders.showOrders(ctx);
+  });
+  
+  bot.action('recover_vouchers', async (ctx) => {
+    await recoverVoucher.start(ctx);
+  });
+  
+  bot.action('support', async (ctx) => {
+    await support.start(ctx);
+  });
+  
+  bot.action('disclaimer', async (ctx) => {
+    await disclaimer.show(ctx);
+  });
+  
+  // Back button
+  bot.action('back_to_main', async (ctx) => {
+    const { showMainMenu } = require('../start');
+    await showMainMenu(ctx);
+  });
+  
+  // Leave button (for support)
+  bot.action('leave_support', async (ctx) => {
+    const { showMainMenu } = require('../start');
+    await ctx.reply('👋 Thanks for contacting support. We\'ll get back to you soon.');
+    await showMainMenu(ctx);
+  });
+}
+
+module.exports = { register };
