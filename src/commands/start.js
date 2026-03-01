efeb7808e06cc8ce57b54855ee6d5e5303cd37b6
Clickdev <clickshop1987@gmail.com>
2026-03-01 08:45:29 +0530

    Update start.js

--- src/commands/start.js
+++ src/commands/start.js
@@ -1,59 +1,83 @@
-const { forceJoinMiddleware } = require('../middlewares/channelCheck');
-const { userExistsMiddleware } = require('../middlewares/auth');
-const constants = require('../utils/constants');
-const helpers = require('../utils/helpers');
+const { Markup } = require('telegraf');
+const { checkMembership } = require('../middlewares/channelCheck');
+const { deleteOldMessage, saveUser } = require('../utils/helpers');
 
-async function startCommand(bot, msg) {
-    const chatId = msg.chat.id;
-    const userId = msg.from.id;
+async function startCommand(ctx) {
+  try {
+    const userId = ctx.from.id;
+    const username = ctx.from.username;
+    const firstName = ctx.from.first_name;
+    const lastName = ctx.from.last_name;
+
+    // Save or update user
+    await saveUser(userId, username, firstName, lastName);
+
+    // Check channel membership
+    const isMember = await checkMembership(ctx);
     
-    // Auto delete previous messages
-    if (global.lastMessages && global.lastMessages[userId]) {
-        try {
-            await bot.deleteMessage(chatId, global.lastMessages[userId]);
-        } catch (error) {
-            // Ignore deletion errors
+    if (!isMember) {
+      const joinButtons = [];
+      
+      if (process.env.CHANNEL_1) {
+        joinButtons.push([Markup.button.url('📢 Join Channel 1', `https://t.me/${process.env.CHANNEL_1.replace('@', '')}`)]);
+      }
+      if (process.env.CHANNEL_2) {
+        joinButtons.push([Markup.button.url('📢 Join Channel 2', `https://t.me/${process.env.CHANNEL_2.replace('@', '')}`)]);
+      }
+      
+      joinButtons.push([Markup.button.callback('✅ Verify Join', 'verify_join')]);
+
+      await ctx.reply(
+        "👋 *Welcome to Shein Codes Bot*\n\n" +
+        "📢 Please join our channels to continue:",
+        {
+          parse_mode: "Markdown",
+          reply_markup: {
+            inline_keyboard: joinButtons
+          }
         }
+      );
+      return;
     }
+
+    // User is a member, show main menu
+    await showMainMenu(ctx);
     
-    // Check channel membership first
-    const membership = await require('../middlewares/channelCheck').checkChannelMembership(bot, userId);
-    
-    if (!membership.joined) {
-        const joinMsg = await bot.sendMessage(chatId, constants.JOIN_MESSAGE, {
-            reply_markup: {
-                inline_keyboard: [
-                    [{ text: '✅ Verify', callback_data: 'verify_channels' }]
-                ]
-            }
-        });
-        global.lastMessages = global.lastMessages || {};
-        global.lastMessages[userId] = joinMsg.message_id;
-        return;
-    }
-    
-    // User is joined, create/update in database
-    await db.createOrUpdateUser({
-        telegram_id: userId,
-        username: msg.from.username,
-        first_name: msg.from.first_name,
-        last_name: msg.from.last_name
-    });
-    
-    // Show main menu
-    const welcomeMsg = await bot.sendMessage(chatId, constants.WELCOME_MESSAGE, {
-        reply_markup: {
-            keyboard: [
-                [constants.MAIN_MENU.BUY, constants.MAIN_MENU.RECOVER],
-                [constants.MAIN_MENU.ORDERS, constants.MAIN_MENU.DISCLAIMER],
-                [constants.MAIN_MENU.SUPPORT]
-            ],
-            resize_keyboard: true,
-            one_time_keyboard: false
-        }
-    });
-    
-    global.lastMessages[userId] = welcomeMsg.message_id;
+  } catch (error) {
+    console.error('Start command error:', error);
+    ctx.reply('An error occurred. Please try again later.');
+  }
 }
 
-module.exports = startCommand;
+async function showMainMenu(ctx) {
+  const message = 
+    "🎯 *Welcome to Shein Voucher Hub!*\n\n" +
+    "🚀 Get exclusive Shein vouchers at the best prices!\n\n" +
+    "📌 *Choose an option below:*";
+
+  const keyboard = Markup.inlineKeyboard([
+    [Markup.button.callback('🛒 Buy Voucher', 'buy_voucher')],
+    [Markup.button.callback('🔁 Recover Vouchers', 'recover_vouchers')],
+    [Markup.button.callback('📦 My Orders', 'my_orders')],
+    [Markup.button.callback('📜 Disclaimer', 'disclaimer')],
+    [Markup.button.callback('🆘 Support', 'support')]
+  ]);
+
+  await ctx.reply(message, {
+    parse_mode: "Markdown",
+    reply_markup: keyboard.reply_markup
+  });
+}
+
+async function handleVerifyJoin(ctx) {
+  const isMember = await checkMembership(ctx);
+  
+  if (isMember) {
+    await deleteOldMessage(ctx);
+    await showMainMenu(ctx);
+  } else {
+    await ctx.answerCbQuery('❌ You have not joined both channels yet!', { show_alert: true });
+  }
+}
+
+module.exports = { startCommand, showMainMenu, handleVerifyJoin };
